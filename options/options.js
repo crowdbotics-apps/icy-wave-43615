2904d02aead4cfaa21a5eb615ae0a2bd587e2aec
Crowdbotics <team@crowdbotics.com>
2023-09-28 17:53:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "icy-wave-43615",
+  url: "https://icy-wave-43615.botics.co",
+  api: "https://icy-wave-43615.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
